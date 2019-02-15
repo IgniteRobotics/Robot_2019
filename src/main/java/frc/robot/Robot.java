a85327fcf23779161a058c2aa173e236ed338d83
shintyl <14168765+shintyl@users.noreply.github.com>
2019-02-15 17:56:50 -0500

    fixed typo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -63,7 +63,7 @@ public class Robot extends TimedRobot {
 
   /**
    * This function is called every robot packet, no matter the mode. Use
-   * this for items like dia"gnostics that you want ran during disabled,
+   * this for items like diagnostics that you want ran during disabled,
    * autonomous, teleoperated and test.
    *
    * <p>This runs after the mode specific periodic functions, but before
